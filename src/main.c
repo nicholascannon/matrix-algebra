b99e450c8bd7bb5ca5b3758ab60fa38c5341728e
Nicholas Cannon <nicholascannon1@gmail.com>
2019-09-12 18:51:34 +0800

    added test data

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/**
+ * CITS3402 Project 1
+ *
+ * Written by Nicholas Cannon (22241579)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int agrv, char* argv) {
+    printf("Hello\n");
+    return 0;
+}
