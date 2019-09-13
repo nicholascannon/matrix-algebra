4a3f409185bb88f1dba5bd4f5108829aa19e98ad
Nicholas Cannon <nicholascannon1@gmail.com>
2019-09-13 15:48:01 +0800

    added matrix structs

--- include/parseMatrix.h
+++ include/parseMatrix.h
@@ -6,4 +6,32 @@
 #ifndef PARSE_MATRIX_HEADER
 #define PARSE_MATRIX_HEADER
 
+/* Coordinate Format struct */
+typedef struct {
+    int rows;
+    int cols;
+    int NZ[];  // non zero elements
+} COO_INT;
+typedef struct {
+    int rows;
+    int cols;
+    float NZ[];  // non zero elements
+} COO_FLOAT;
+
+/* Compressed Format struct (used for both compressed col and row) */
+typedef struct {
+    int rows;
+    int cols;
+    int NNZ[];  // non-zero elments
+    int IA[];
+    int JA[];
+} CS_INT;
+typedef struct {
+    int rows;
+    int cols;
+    float NNZ[];  // non-zero elments
+    int IA[];
+    int JA[];
+} CS_FLOAT;
+
 #endif
